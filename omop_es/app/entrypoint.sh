0b71ae6654e216e0f3161bd6da42a5c6bdbf12fb
BaptisteBR <baptiste.briot@gmail.com>
2024-01-26 18:10:38 +0000

    Fix OMOP_ES batch extracts

--- omop_es/app/entrypoint.sh
+++ omop_es/app/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#Rscript /app/test_libraries.R
+# Rscript /app/test_libraries.R
 URL="https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_URL}"
 DIR="/omop_es"
 OUT="/share/criudata/crdm-tools/omop_es/$CRDM_ID/$(date +"%Y%m%d%H%M%S")"
 [ -d "$DIR" ] && git -C "$DIR" pull "$URL"
 [ ! -d "$DIR" ] && git clone "$URL" "$DIR" && cd /omop_es && Rscript env_setup/download_omop_metadata.R
 cd /omop_es
-Rscript main_command.R --id $CRDM_ID
+if $CRDM_BATCH; then
+    Rscript main_batched.R --id $CRDM_ID
+else
+    Rscript main_command.R --id $CRDM_ID
+fi
 mkdir -p $OUT
-mv /extra* $OUT
+mv /share/local/* $OUT
