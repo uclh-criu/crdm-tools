c31201f14bad50c06ba89a25829456651cf4cb08
BaptisteBR <baptiste.briot@gmail.com>
2024-10-17 12:18:26 +0100

    Add arguments for OMOP_ES batched process. Rework the omop-cascade container to use R-4.4.1. Rework entrypoints.

--- omop-cascade/app/entrypoint.sh
+++ omop-cascade/app/entrypoint.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+#
+# Used to debug R libraries
 # Rscript /app/test_libraries.R
-URL="https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_URL}"
-DIR="/omop-cascade"
-[ -d "$DIR" ] && git -C "$DIR" pull "$URL"
-[ ! -d "$DIR" ] && git clone "$URL" "$DIR"
-cd /omop-cascade
-Rscript import_omop.R --id $CRDM_ID
+#
+# Define all variables
+# Git variables are coming from the '.env' file
+FULL_GIT_URL="https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_URL}"
+OMOP_CASCADE_DIR="/omop-cascade"
+# The following variable is relative to the omop-cascade directory
+IMPORT_OMOP="./import_omop.R"
+#
+# Clone the GitHub repo if it doesn't exist,
+# otherwise pull the latest version of the specified branch
+if [ ! -d $OMOP_CASCADE_DIR ]; then
+    git clone -b $GIT_BRANCH $FULL_GIT_URL $OMOP_CASCADE_DIR
+else
+    git -C $OMOP_CASCADE_DIR pull $FULL_GIT_URL $GIT_BRANCH
+fi
+#
+# Move to the omop-cascade directory
+cd $OMOP_CASCADE_DIR
+#
+# Run the R script to import an OMOP parquet dataset
+# into a database
+# The CRDM_ID is coming from the 'docker compose up' command line
+# and sent to the container in the 'docker-compose.yml' file
+Rscript $IMPORT_OMOP --id $CRDM_ID
--- omop_es/app/entrypoint.sh
+++ omop_es/app/entrypoint.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
+#
+# Used to debug R libraries
 # Rscript /app/test_libraries.R
-URL="https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_URL}"
-DIR="/omop_es"
-OUT="/sharefs6/criudata/live/crdm-tools/omop_es/$CRDM_ID/$(date +"%Y%m%d%H%M%S")"
-[ -d "$DIR" ] && git -C "$DIR" pull "$URL" "$GIT_BRANCH"
-[ ! -d "$DIR" ] && git clone -b "$GIT_BRANCH" "$URL" "$DIR" && cd /omop_es && Rscript omop_metadata/download_omop_metadata.R
-cd /omop_es
-if $CRDM_BATCH; then
-    Rscript main_batched.R --id $CRDM_ID
+#
+# Define all variables
+# Git variables are coming from the '.env' file
+FULL_GIT_URL="https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_URL}"
+OMOP_ES_DIR="/omop_es"
+# The following variables are relative to the OMOP_ES directory
+METADATA_VERSION="./omop_metadata/metadata_version.txt"
+METADATA_DOWNLOAD="./omop_metadata/download_omop_metadata.R"
+MAIN_BATCHED="./main_batched.R"
+MAIN_COMMAND="./main_command.R"
+#
+# Clone the GitHub repo if it doesn't exist,
+# otherwise pull the latest version of the specified branch
+if [ ! -d $OMOP_ES_DIR ]; then
+    git clone -b $GIT_BRANCH $FULL_GIT_URL $OMOP_ES_DIR
 else
-    Rscript main_command.R --id $CRDM_ID
+    git -C $OMOP_ES_DIR pull $FULL_GIT_URL $GIT_BRANCH
 fi
-mkdir -p $OUT
-mv /share/local/* $OUT
+#
+# Move to the OMOP_ES directory
+cd $OMOP_ES_DIR
+#
+# Download the metadata files if they don't exist
+if [ ! -f $METADATA_VERSION ]; then
+    Rscript $METADATA_DOWNLOAD
+fi
+#
+# Run the batched process if specified otherwise run the simple process
+# All the variables prefixed with CRDM are coming from the 'docker compose up' command line,
+# and sent to the container in the 'docker-compose.yml' file
+if $CRDM_BATCH; then
+    Rscript $MAIN_BATCHED --id $CRDM_ID --zip $CRDM_ZIP --batch $CRDM_START --dir $CRDM_DIR
+else
+    Rscript $MAIN_COMMAND --id $CRDM_ID
+fi
