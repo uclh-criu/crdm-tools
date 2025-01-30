a723a322c6ae4afb59ca1f84110fe154cd9bb922
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-01-30 15:59:31 +0000

    Add some logging to entrypoint script

--- omop_es/app/entrypoint.sh
+++ omop_es/app/entrypoint.sh
@@ -16,24 +16,28 @@ MAIN_COMMAND="./main_command.R"
 # Clone the GitHub repo if it doesn't exist,
 # otherwise pull the latest version of the specified branch
 if [ ! -d $OMOP_ES_DIR ]; then
+	echo "Cloning GitHub repo ${GITHUB_REPO}@${GIT_BRANCH}"
 	git clone -b $GIT_BRANCH $FULL_GIT_URL $OMOP_ES_DIR
 else
+	echo "Pulling GitHub repo ${GITHUB_REPO}@${GIT_BRANCH}"
 	git -C $OMOP_ES_DIR pull $FULL_GIT_URL $GIT_BRANCH
 fi
-#
+
 # Move to the OMOP_ES directory
 cd $OMOP_ES_DIR
-#
+
 # Download the metadata files if they don't exist
 if [ ! -f $METADATA_VERSION ]; then
 	Rscript $METADATA_DOWNLOAD
 fi
-#
+
 # Run the batched process if specified otherwise run the simple process
 # All the variables prefixed with OMOP_ES are coming from the 'docker compose up' command line,
 # and sent to the container in the 'docker-compose.yml' file
 if $OMOP_ES_BATCHED; then
+	echo "Running batched omop_es for ${OMOP_ES_SETTINGS_ID}..."
 	Rscript $MAIN_BATCHED --settings_id $OMOP_ES_SETTINGS_ID --start_batch $OMOP_ES_START_BATCH --extract_dt $OMOP_ES_EXTRACT_DT
 else
+	echo "Running omop_es for ${OMOP_ES_SETTINGS_ID}..."
 	Rscript $MAIN_COMMAND --settings_id $OMOP_ES_SETTINGS_ID --zip_output $OMOP_ES_ZIP_OUTPUT
 fi
