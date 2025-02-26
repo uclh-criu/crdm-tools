714606782f1cc377562b01631bfaf8a752a22ca0
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-02-26 12:23:23 +0000

    Fix omop-cascade script with new envvars

--- docker/omop-cascade.sh
+++ docker/omop-cascade.sh
@@ -7,16 +7,18 @@ set -euxo pipefail
 
 # Define all variables
 # Git variables are coming from the '.env' file
-FULL_GIT_URL="https://${GITHUB_PAT}@${GIT_URL}"
-OMOP_CASCADE_DIR="/omop-cascade"
+FULL_GIT_URL="https://${GITHUB_PAT}@github.com/${GITHUB_REPO}.git"
+OMOP_CASCADE_DIR="omop-cascade"
 # The following variable is relative to the omop-cascade directory
 IMPORT_OMOP="./import_omop.R"
 
 # Clone the GitHub repo if it doesn't exist,
 # otherwise pull the latest version of the specified branch
 if [ ! -d $OMOP_CASCADE_DIR ]; then
+	echo "Cloning GitHub repo ${GITHUB_REPO}@${GIT_BRANCH}"
 	git clone -b $GIT_BRANCH $FULL_GIT_URL $OMOP_CASCADE_DIR
 else
+	echo "Pulling GitHub repo ${GITHUB_REPO}@${GIT_BRANCH}"
 	git -C $OMOP_CASCADE_DIR pull $FULL_GIT_URL $GIT_BRANCH
 fi
 
