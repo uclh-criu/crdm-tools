9469d964574a1cea117ea351329b3e999bd61f93
Stef Piatek <s.piatek@ucl.ac.uk>
2026-03-18 15:09:37 +0000

    Run stderr parsing in a separate thread

--- docker-poc/src/docker/main.py
+++ docker-poc/src/docker/main.py
@@ -28,7 +28,7 @@ app = typer.Typer()
 @app.command()
 def write_message(message: str, filename: Path = Path("output.txt")):
     """Writes the message and current timestamp to a file 25% of the time, raises error otherwise"""
-    for i in range(200):
+    for i in range(2000):
         sleep(0.1)
         logger.debug(f"Some logging output {i}")
 
